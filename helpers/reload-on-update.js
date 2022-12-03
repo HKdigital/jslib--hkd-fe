07de77679f7510c559228a981c5abde6ff3de72f
Jens Kleinhout <contact@hkdigital.nl>
2022-12-03 10:46:55 +0100

    helpers/reload-on-update.js: enabled periodic check, refined check

--- helpers/reload-on-update.js
+++ helpers/reload-on-update.js
@@ -2,34 +2,42 @@
 /* ------------------------------------------------------------------ Imports */
 
 import { httpGet } from "@hkd-base/helpers/http.js";
+import { delay } from "@hkd-base/helpers/time.js";
+import { defer } from "@hkd-base/helpers/process.js";
 
 /* ---------------------------------------------------------------- Internals */
 
 const MIN_CHECK_INTERVAL = 10000;
 
+const MIN_CHECK_LOOP_DELAY = 50000;
+
 let lastCheckedAt = 0;
+
+const ORIGIN = location.origin;
+const ORIGIN_ASSETS_PREFIX = `${ORIGIN}/assets`;
+
+let RELOAD_MESSAGE = "Update available. Reload page?";
 
 /* ------------------------------------------------------------------ Exports */
 
 /**
  * Enable check for updates
+ *
+ * @param {string} customReloadMessage
  */
-export function enableCheckForUpdates()
+export async function enableCheckForUpdates()
 {
-
-  // console.log( 123, document.visibilityState );
+  // == Check if app becomes visible
 
   document.addEventListener("visibilitychange", () => {
 
-    // console.log("456", document.visibilityState );
-
     if( "visible" === document.visibilityState )
     {
-      // alert("visble");
       tryAutoUpdate();
     }
   } );
 
+  // == Check after boot
 
   setTimeout( () =>
     {
@@ -37,37 +45,57 @@ export function enableCheckForUpdates()
     },
     2000 );
 
-  // setInterval( () =>
-  //   {
-  //     tryAutoUpdate();
-  //   },
-  //   60000 );
+  // == Start check loop
+
+  checkLoop();
+}
+
+// -----------------------------------------------------------------------------
+
+/**
+ * Infinite check loop
+ * - Waits a variable amount of time
+ * - Checks if an update is available
+ */
+async function checkLoop()
+{
+  try {
+    await delay( MIN_CHECK_LOOP_DELAY, MIN_CHECK_LOOP_DELAY * 1.2 );
+
+    if( "visible" === document.visibilityState && navigator.onLine !== false )
+    {
+      tryAutoUpdate();
+    }
+  }
+  catch( e ) { console.log( e ); }
+
+  defer( checkLoop );
 }
 
 // -----------------------------------------------------------------------------
 
 /**
  * Check if there is a new version of the script available
- * - Checks script urls in the index.html
+ * - Checks if asset urls in the index.html have changed
  */
-async function tryAutoUpdate( force=false )
+async function tryAutoUpdate()
 {
+  // console.log("tryAutoUpdate");
+
   const now = Date.now();
 
   if( now < lastCheckedAt + MIN_CHECK_INTERVAL )
   {
-    // Recently checked
+    // Recently checked => ignore
     return;
   }
 
   lastCheckedAt = now;
 
-  // alert("check");
-
   try {
-    let assetSources = [];
+    // == Get list of current SCRIPT assets on the page
 
-    let origin = location.origin;
+    let assetSources = [];
 
     const scripts = document.scripts;
 
@@ -77,41 +105,40 @@ async function tryAutoUpdate( force=false )
 
       // console.log( src );
 
-      if( src.startsWith( origin ) )
+      if( src.startsWith( ORIGIN_ASSETS_PREFIX ) )
       {
-        const uri = src.slice( origin.length + 1);
+        const path = src.slice( ORIGIN.length );
 
-        if( uri !== "@vite/client" )
+        assetSources.push( path );
+
+        const index = path.indexOf("assets/index.");
+
+        if( index >= 0 /*&& path.endsWith(".js")*/ )
         {
-          // console.log( uri );
-
-          assetSources.push( uri );
-
-          const index = uri.indexOf("assets/index.");
-
-          if( index >= 0 )
-          {
-            window.scriptVersion = uri.slice( index + 13, index + 21 );
-
-            console.log(`version ${window.scriptVersion}`);
-          }
+          window.scriptVersion = path.slice( index + 13, index + 21 );
+          console.log(`Current site version [${window.scriptVersion}]`);
         }
       }
     } // end for
 
     // console.log( "assets", assetSources );
 
-    if( assetSources.length || 1 === 1 )
+    // == Fetch latest "index.html"
+
+    if( assetSources.length )
     {
+      //
+      // The page has script assets => check
+      //
+
       const response =
         await httpGet(
           {
-            url: new URL( "index.html", location.origin )
+            // url: new URL( "index.html", location.origin )
+            url: location.href
           } );
 
       const content = await response.text();
-
-      // console.log( "On server", content );
 
       for( let j = 0, n = assetSources.length; j < n; j = j + 1 )
       {
@@ -120,16 +147,20 @@ async function tryAutoUpdate( force=false )
         if( !content.includes( src ) )
         {
           console.log(
-            "New version of index.html is available", { reason: src } );
+            "New site version is available",
+            { reason: src }
+            /*,content*/ );
 
-          if( confirm("Update available. Reload page?") )
+          if( confirm( RELOAD_MESSAGE ) )
           {
             location.reload();
           }
-
         }
       }
     }
+    // else {
+    //   console.log("tryAutoUpdate: skip check (no assets on page)");
+    // }
   }
   catch( e )
   {
