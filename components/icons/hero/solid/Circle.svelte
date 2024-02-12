d78e282e4ce874b9ab9a41f2353674c70e7f0057
Jens Kleinhout <contact@hkdigital.nl>
2024-02-12 18:23:11 -0300

    Added hero icon (solid) Circle

---
+++ components/icons/hero/solid/Circle.svelte
@@ -0,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Z" clip-rule="evenodd" />
+</svg>
